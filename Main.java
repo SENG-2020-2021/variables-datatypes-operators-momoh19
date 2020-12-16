8d7e24d601be6907b4381f890e8e81494b15829e
momoh19 <momoh2536@student.babcock.edu.ng>
2020-12-16 20:06:59 +0000

    Vdo Assignment submission

--- Main.java
+++ Main.java
@@ -1,21 +1,70 @@
-class Main {
-  public static void main(String[] args) {
-   // kindly use appropriate data types for your declaration
-   // declare variables days the debtor defaulted, amount the debtor is to pay per day,amount the debtor paid and assign values to them
-   
-   //Declare the variable of the total amount the debtor is to pay, the days the debtor paid for, amount the debtor is left to pay, days the debtor has not payed for
+class Main{
+    public static void main(String[] args) {
+     // kindly use appropriate data types for your declaration
+     //declare an array variable and assign the days of the month debtor defaulted,
 
-   // calculate and print total amount the debtor is to pay
+     byte []daysDefaulted={10,12,14,16,18};
+     
+     // declare variables of amount the debtor is to pay per day, and amount the debtor paid and assign values to them
+    
+     float amountPerDay=500f;
+     float amountPaid=2650f;
 
-   // calculate and print the days the debtor paid for
-  
-  // calculate and print the amount whose day was not captured because the amount was incomplete
+    // declare all the variables needed for your calculations
 
-   // calculate and print amount the debtor is left to pay
+     float totalAmtToPay=0f;
+     byte daysPaidFor;
+     float incompleteAmount=0f;
+     float amountLeftToPay=0f;
+     byte daysNotPaidFor;
+     float extraAmount=0f;
+     byte numOfDaysDefaulted=5;
+    
 
-   // calculate and print days the debtor has not payed for
+    // calculate and print total amount the debtor is to pay using for each loop
+			for(byte x: daysDefaulted){
+				totalAmtToPay=totalAmtToPay+amountPerDay;
+			}
+			  System.out.println("Total amount to pay is "+totalAmtToPay);
+			  System.out.println();
+		 
+    // calculate the days the debtor paid for and print the days in which was paid for starting from the left to the right of the array. Also check if there was a day that the debtor did not pay the complete amount for that day, if yes, print the day and the amount left to pay for that day. 
+			
+			daysPaidFor=(byte)(amountPaid/amountPerDay);
+			  System.out.println("Days paid for is "+daysPaidFor+" days");
+			  System.out.println();
+		   
+	        for(int i=0; i<numOfDaysDefaulted&&i<daysPaidFor; i++){
+              System.out.println("Day "+daysDefaulted[i]+" has been paid for");    
+	          System.out.println();
+		    }
+		  
+	        if(amountPaid%amountPerDay>0)  
+			  incompleteAmount=amountPerDay-(amountPaid%amountPerDay);
+			  System.out.println("The Incomplete amount in day "+daysDefaulted[daysPaidFor +1]+ " left to pay is "+incompleteAmount);   
+			  System.out.println(" ");
 
-   //kindly remove the statement below when you are done with the assignment
-    System.out.println("Hello world!");
+    // calculate amount the debtor is left to pay if he did not pay complete fees and the days not paid for or  check if the debtor paid extra and the extra amount he paid for, or if he paid the 	exact amount needed to be paid and print likewise
+	
+			if(totalAmtToPay>amountPaid) { 
+              amountLeftToPay=totalAmtToPay-amountPaid;
+              System.out.println("The amount left to pay is "+amountLeftToPay);
+			  System.out.println();
+
+			  daysNotPaidFor=(byte)(numOfDaysDefaulted-daysPaidFor);
+			  System.out.println("Days not paid for are "+daysNotPaidFor+" day"); 
+			  System.out.println();
+			}	
+			else if(totalAmtToPay<amountPaid){
+		      extraAmount=amountPaid-totalAmtToPay;
+			  System.out.println("Extra amount of "+extraAmount+" was paid");
+			  System.out.println();
+	        }
+	   
+	        else if(totalAmtToPay==amountPaid){
+			  System.out.println("The exact amount was paid");
+	        }
+        
+
   }
-}
+  }
